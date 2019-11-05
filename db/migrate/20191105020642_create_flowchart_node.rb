f19f329872aec192a6a169ccd8b6939ddda2a89f
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-11-04 22:04:49 -0500

    fix linter errors

--- db/migrate/20191105020642_create_flowchart_node.rb
+++ db/migrate/20191105020642_create_flowchart_node.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateFlowchartNode < ActiveRecord::Migration[6.0]
   def change
     create_table :flowchart_nodes do |t|
@@ -5,10 +7,10 @@ class CreateFlowchartNode < ActiveRecord::Migration[6.0]
       t.string :header, null: false
       t.string :button_text
       t.string :next_question
-      t.references :child_id, foreign_key: {to_table: :flowchart_nodes}
-      t.references :sibling_id, foreign_key: {to_table: :flowchart_nodes}
+      t.references :child_id, foreign_key: { to_table: :flowchart_nodes }
+      t.references :sibling_id, foreign_key: { to_table: :flowchart_nodes }
       t.boolean :is_root, null: false
-      t.references :flowchart_id, foreign_key: {to_table: :flowcharts}, null: false
+      t.references :flowchart_id, foreign_key: { to_table: :flowcharts }, null: false
       t.timestamps
     end
   end
