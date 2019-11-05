2a12dd714e2813d78b49ace06b7a8c7a12bd4e81
Daniel Peng <danielpeng2@gmail.com>
2019-11-04 23:40:23 -0500

    Create flowchart node migration (#17)
    * create flowchart node migration
    
    * fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_020547) do
+ActiveRecord::Schema.define(version: 2019_11_05_020642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flowchart_nodes", force: :cascade do |t|
+    t.string "text", null: false
+    t.string "header", null: false
+    t.string "button_text"
+    t.string "next_question"
+    t.bigint "child_id_id"
+    t.bigint "sibling_id_id"
+    t.boolean "is_root", null: false
+    t.bigint "flowchart_id_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["child_id_id"], name: "index_flowchart_nodes_on_child_id_id"
+    t.index ["flowchart_id_id"], name: "index_flowchart_nodes_on_flowchart_id_id"
+    t.index ["sibling_id_id"], name: "index_flowchart_nodes_on_sibling_id_id"
+  end
 
   create_table "flowcharts", force: :cascade do |t|
     t.string "title", null: false
@@ -28,4 +44,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_020547) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "flowchart_nodes", "flowchart_nodes", column: "child_id_id"
+  add_foreign_key "flowchart_nodes", "flowchart_nodes", column: "sibling_id_id"
+  add_foreign_key "flowchart_nodes", "flowcharts", column: "flowchart_id_id"
 end
