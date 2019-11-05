7ca8fe6af10940428f3ba279b7416e6d89b7000c
Jayant Shrivastava <jshrivastava03@gmail.com>
2019-11-04 21:36:54 -0500

    Linting

--- db/migrate/20191105020547_create_flowcharts.rb
+++ db/migrate/20191105020547_create_flowcharts.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateFlowcharts < ActiveRecord::Migration[6.0]
   def change
     create_table :flowcharts do |t|
       t.string :title, null: false
-      t.string :description, null: false 
+      t.string :description, null: false
       t.integer :height, null: false
       t.timestamps
     end
