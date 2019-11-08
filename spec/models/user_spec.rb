cd4e23591a2792ad3e1a0a025720d6550f51f531
lee-ma <leema9321@gmail.com>
2019-11-07 20:49:18 -0500

    fix linting in rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,23 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with valid attributes" do
-    expect(User.new(email: "test@test.com", password:"password")).to be_valid
+  it 'is valid with valid attributes' do
+    expect(User.new(email: 'test@test.com', password: 'password')).to be_valid
   end
-  it "is not valid without email" do
+  it 'is not valid without email' do
     user = User.new(email: nil)
     expect(user).to_not be_valid
   end
-  it "is not valid without encrypted password" do
+  it 'is not valid without encrypted password' do
     user = User.new(password: nil)
     expect(user).to_not be_valid
   end
-  it "is not valid with invalid email" do
-    user = User.new(email: "not_an_email", password: "testing")
+  it 'is not valid with invalid email' do
+    user = User.new(email: 'not_an_email', password: 'testing')
     expect(user).to_not be_valid
   end
-  it "is not valid with password shorter than 6 chars" do
-    user = User.new(email: "test@test.com", password: "p")
+  it 'is not valid with password shorter than 6 chars' do
+    user = User.new(email: 'test@test.com', password: 'p')
     expect(user).to_not be_valid
   end
 end
