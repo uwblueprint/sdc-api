ac9a2ab4505788a50e7f78cdab532caebada8093
lee-ma <leema9321@gmail.com>
2019-11-07 20:47:12 -0500

    add rspec tests and specify rails env for circleci

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(User.new(email: "test@test.com", password:"password")).to be_valid
+  end
+  it "is not valid without email" do
+    user = User.new(email: nil)
+    expect(user).to_not be_valid
+  end
+  it "is not valid without encrypted password" do
+    user = User.new(password: nil)
+    expect(user).to_not be_valid
+  end
+  it "is not valid with invalid email" do
+    user = User.new(email: "not_an_email", password: "testing")
+    expect(user).to_not be_valid
+  end
+  it "is not valid with password shorter than 6 chars" do
+    user = User.new(email: "test@test.com", password: "p")
+    expect(user).to_not be_valid
+  end
 end
