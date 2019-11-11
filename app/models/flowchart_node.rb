a33b743b63759071260f26b59ffb1eabc7653fc9
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-11-11 16:03:36 -0500

    [WIP]: show, update, and swap functions

--- app/models/flowchart_node.rb
+++ app/models/flowchart_node.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class FlowchartNode < ApplicationRecord
+  belongs_to :flowchart
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/questions', to: 'questions#index'
+
   get '/flowchart/:id', to: 'flowchart#serialized_flowchart_by_id'
   get '/flowcharts', to: 'flowchart#all_flowcharts'
   post '/flowchart', to: 'flowchart#create'
   put '/flowchart/:id', to: 'flowchart#update'
   delete '/flowchart/:id', to: 'flowchart#delete'
+
+  get '/flowchart_node/:id', to: 'flowchart_node#show'
+  put '/flowchart_node/swap', to: 'flowchart_node#swap'
+  put '/flowchart_node/:id', to: 'flowchart_node#update'
+  delete '/flowchart_node/:id', to: 'flowchart_node#delete'
 end
+
