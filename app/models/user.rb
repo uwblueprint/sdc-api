1d37c0caa1854d0180a54d7f66fbf33ee583926a
lee-ma <leema9321@gmail.com>
2019-11-11 21:27:37 -0500

    remove comments from user model code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :validatable
 end
