b4ee862a61478460662586b78a432e0b7d8c5060
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-11-13 00:07:09 -0500

    add soft delete in node, run rubocop

--- app/models/flowchart_node.rb
+++ app/models/flowchart_node.rb
@@ -2,4 +2,13 @@
 
 class FlowchartNode < ApplicationRecord
   belongs_to :flowchart
+
+  def soft_delete
+    self.deleted = true
+    save
+    sibling = FlowchartNode.find_by(id: sibling_id)
+    child = FlowchartNode.find_by(id: child_id)
+    sibling&.soft_delete
+    child&.soft_delete
+  end
 end
