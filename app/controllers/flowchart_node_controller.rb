20e4d67daddcf09930286a1ce3da6cbcac663277
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-11-16 16:01:23 -0500

    Updates to swap/delete and unit tests

--- app/controllers/flowchart_node_controller.rb
+++ app/controllers/flowchart_node_controller.rb
@@ -36,48 +36,54 @@ class FlowchartNodeController < ApplicationController
   def swap
     id_a = params[:id_a]
     id_b = params[:id_b]
-    node_a = FlowchartNode.find_by(id: id_a)
-    node_b = FlowchartNode.find_by(id: id_b)
-    if !node_a || !node_b
+    begin
+      node_a = FlowchartNode.find(id_a)
+      node_b = FlowchartNode.find(id_b)
+    rescue ActiveRecord::RecordNotFound
       render status: 404, json: { error: 'Error finding nodes with the given ids.' }
     else
-      node_a[:text], node_b[:text] = node_b[:text], node_a[:text]
-      node_a[:header], node_b[:header] = node_b[:header], node_a[:header]
-      node_a[:button_text], node_b[:button_text] = node_b[:button_text], node_a[:button_text]
-      node_a[:next_question], node_b[:next_question] = node_b[:next_question], node_a[:next_question]
-      node_a[:is_root], node_b[:is_root] = node_b[:is_root], node_a[:is_root]
-      node_a[:child_id], node_b[:child_id] = node_b[:child_id], node_a[:child_id]
+      node_a.text, node_b.text = node_b.text, node_a.text
+      node_a.header, node_b.header = node_b.header, node_a.header
+      node_a.button_text, node_b.button_text = node_b.button_text, node_a.button_text
+      node_a.next_question, node_b.next_question = node_b.next_question, node_a.next_question
+      node_a.is_root, node_b.is_root = node_b.is_root, node_a.is_root
+      node_a.child_id, node_b.child_id = node_b.child_id, node_a.child_id
       ActiveRecord::Base.transaction do
         node_a.save!
         node_b.save!
       end
+      render status: 200, json: { new_a: node_a.as_json, new_b: node_b.as_json }
     end
-    render status: 200, json: { new_a: node_b.as_json, new_b: node_a.as_json }
   end
 
   def delete
     delete_id = params[:id]
-    parent_node = FlowchartNode.find_by(child_id: delete_id)
-    left_node = FlowchartNode.find_by(sibling_id: delete_id)
-    delete_node = FlowchartNode.find_by(id: delete_id)
-    child_node = FlowchartNode.find_by(id: delete_node[:child_id])
-    delete_node[:deleted] = true
-    if !parent_node && !left_node
-      delete_node.save!
-    elsif !parent_node
-      left_node[:sibling_id] = delete_node[:sibling_id]
-      ActiveRecord::Base.transaction do
-        left_node.save!
-        delete_node.save!
-      end
+    begin
+      delete_node = FlowchartNode.find(delete_id)
+    rescue ActiveRecord::RecordNotFound
+      render status: 404, json: { error: "No node found with id #{delete_id}." }
     else
-      parent_node[:child_id] = delete_node[:sibling_id]
-      ActiveRecord::Base.transaction do
-        parent_node.save!
+      parent_node = FlowchartNode.find_by(child_id: delete_id)
+      left_node = FlowchartNode.find_by(sibling_id: delete_id)
+      child_node = FlowchartNode.find_by(id: delete_node.child_id)
+      delete_node.deleted = true
+      if !parent_node && !left_node
         delete_node.save!
+      elsif !parent_node
+        left_node.sibling_id = delete_node.sibling_id
+        ActiveRecord::Base.transaction do
+          left_node.save!
+          delete_node.save!
+        end
+      else
+        parent_node.child_id = delete_node.sibling_id
+        ActiveRecord::Base.transaction do
+          parent_node.save!
+          delete_node.save!
+        end
       end
+      child_node&.soft_delete
+      render status: 200, json: delete_node.as_json
     end
-    child_node&.soft_delete
-    render status: 200, json: delete_node.as_json
   end
 end
--- spec/controllers/flowchart_node_controller_spec.rb
+++ spec/controllers/flowchart_node_controller_spec.rb
@@ -136,6 +136,12 @@ RSpec.describe FlowchartNodeController, type: :controller do
     context 'when given a valid id' do
       before(:each) do
         @params[:id] = 1
+
+        @expected = @node_1
+        @expected.text = 'mock text'
+        @expected.header = 'mock header'
+        @expected.button_text = 'mock button text'
+        @expected.next_question = 'mock next question'
       end
 
       it 'returns status code 200' do
@@ -143,14 +149,15 @@ RSpec.describe FlowchartNodeController, type: :controller do
         expect(response.response_code).to eq(200)
       end
 
-      it 'renders the json of the updated node' do
-        expected = @node_1
-        expected.text = 'mock text'
-        expected.header = 'mock header'
-        expected.button_text = 'mock button text'
-        expected.next_question = 'mock next question'
+      it 'updates the node in the database' do
         put :update, params: @params
-        expect(JSON.parse(response.body).except(*@exclude_keys)).to eq(expected.attributes.except(*@exclude_keys))
+        updated_node = FlowchartNode.find(1)
+        expect(updated_node.attributes.except(*@exclude_keys)).to eq(@expected.attributes.except(*@exclude_keys))
+      end
+
+      it 'renders the json of the updated node' do
+        put :update, params: @params
+        expect(JSON.parse(response.body).except(*@exclude_keys)).to eq(@expected.attributes.except(*@exclude_keys))
       end
     end
 
@@ -158,7 +165,7 @@ RSpec.describe FlowchartNodeController, type: :controller do
       before(:each) do
         @params[:id] = 100
       end
-      
+
       it 'returns status code 404' do
         put :update, params: @params
         expect(response.response_code).to eq(404)
@@ -167,6 +174,143 @@ RSpec.describe FlowchartNodeController, type: :controller do
       it 'renders the error json' do
         error_json = { :error => 'No node found with id 100.' }.to_json
         put :update, params: @params
+        expect(response.body).to eq(error_json)
+      end
+    end
+  end
+
+  describe '.swap' do
+    context 'when given valid ids' do
+      before(:each) do
+        @params = {
+          id_a: '3',
+          id_b: '4',
+        }
+        @expected = {
+          'new_a' => {
+            'id' => 3,
+            'text' => "d",
+            'header' => "dd",
+            'button_text' => "ddd",
+            'next_question' => "dddd",
+            'child_id' => nil,
+            'sibling_id' => 4,
+            'is_root' => false,
+            'flowchart_id' => 100,
+            'deleted' => false
+          },
+          'new_b' => {
+            'id' => 4,
+            'text' => "c",
+            'header' => "cc",
+            'button_text' => "ccc",
+            'next_question' => "cccc",
+            'child_id' => 6,
+            'sibling_id' => 5,
+            'is_root' => false,
+            'flowchart_id' => 100,
+            'deleted' => false
+          }
+        }
+      end
+
+      it 'returns status code 200' do
+        put :swap, params: @params
+        expect(response.response_code).to eq(200)
+      end
+
+      it 'renders json containing the swapped nodes' do
+        put :swap, params: @params
+        res = JSON.parse(response.body)
+        res['new_a'].delete('updated_at')
+        res['new_a'].delete('created_at')
+        res['new_b'].delete('updated_at')
+        res['new_b'].delete('created_at')
+        expect(res).to eq(@expected)
+      end
+
+      it 'updates node A with the contents of node B in the database' do
+        put :swap, params: @params
+        node_a = FlowchartNode.find(3)
+        expect(node_a.attributes.except(*@exclude_keys)).to eq(@expected['new_a'])
+      end
+
+      it 'updates node B with the contents of node A in the database' do
+        put :swap, params: @params
+        node_b = FlowchartNode.find(4)
+        expect(node_b.attributes.except(*@exclude_keys)).to eq(@expected['new_b'])
+      end
+    end
+
+    context 'when given an invalid id' do
+      before(:each) do
+        @params = {
+          id_a: '1',
+          id_b: '200',
+        }
+      end
+
+      it 'returns status code 404' do
+        put :swap, params: @params
+        expect(response.response_code).to eq(404)
+      end
+
+      it 'renders the error json' do
+        error_json = { :error => 'Error finding nodes with the given ids.' }.to_json
+        put :swap, params: @params
+        expect(response.body).to eq(error_json)
+      end
+    end
+  end
+
+  describe '.delete' do
+    context 'when given a valid id' do
+      it 'returns status code 200' do
+        delete :delete, params: { id: 3 }
+        expect(response.response_code).to eq(200)
+      end
+
+      it 'renders json of the deleted node' do
+        delete :delete, params: { id: 3 }
+        deleted_node = FlowchartNode.find(3)
+        expect(deleted_node.deleted).to be true
+      end
+
+      it 'soft deletes the subtree below the deleted node' do
+        delete :delete, params: { id: 3 }
+        node_6 = FlowchartNode.find(6)
+        node_7 = FlowchartNode.find(7)
+        node_8 = FlowchartNode.find(8)
+        deleted_status = [node_6.deleted, node_7.deleted, node_8.deleted]
+        expect(deleted_status).to match_array([true, true, true])
+      end
+
+      it 'updates the parent\'s child_id if a parent exists' do
+        delete :delete, params: { id: 3 }
+        parent_node = FlowchartNode.find(2)
+        expect(parent_node.child_id).to be 4
+      end
+
+      it 'updates the left sibling\'s sibling_id if a left sibling exists' do
+        delete :delete, params: { id: 4 }
+        sibling_node = FlowchartNode.find(3)
+        expect(sibling_node.sibling_id).to be 5
+      end
+    end
+
+    context 'when given an invalid id' do
+      before(:each) do
+        @params = { id: '100'}
+      end
+
+      it 'returns status code 404' do
+        delete :delete, params: @params
+        expect(response.response_code).to eq(404)
+      end
+
+      it 'renders the error json' do
+        error_json = { :error => 'No node found with id 100.' }.to_json
+        delete :delete, params: @params
         expect(response.body).to eq(error_json)
       end
     end
