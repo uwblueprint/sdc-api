707a5d163c65b1f0142c920079b0860682545636
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2019-11-17 12:00:22 -0500

    linting

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get '/questions', to: 'questions#index'
 
   get '/flowchart_node/:id', to: 'flowchart_node#show'
-  post '/flowchart_node/:id', to: 'flowchart_node#create'
   put '/flowchart_node/swap', to: 'flowchart_node#swap'
   put '/flowchart_node/:id', to: 'flowchart_node#update'
   delete '/flowchart_node/:id', to: 'flowchart_node#delete'
--- spec/models/flowchart_node_model_spec.rb
+++ spec/models/flowchart_node_model_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe FlowchartNode, type: :model do
 
   it 'is valid with valid attributes' do
     expect(FlowchartNode.create(
-      id: 1,
-      text: "text",
-      header: "header",
-      button_text: "button_text",
-      next_question: "next_question",
-      is_root: true,
-      flowchart_id: 100
-    )).to be_valid
+             id: 1,
+             text: 'text',
+             header: 'header',
+             button_text: 'button_text',
+             next_question: 'next_question',
+             is_root: true,
+             flowchart_id: 100
+           )).to be_valid
   end
 
   it 'is not valid without required attributes' do
