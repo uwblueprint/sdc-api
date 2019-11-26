d7700672c4bd714ec9d0e2d8b1939babdbac7188
lee-ma <leema9321@gmail.com>
2019-11-25 23:21:49 -0500

    linting sessions controller comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,13 @@ class SessionsController < Devise::SessionsController
   respond_to :json
 
   private
+
   # this returns a response with the session info in json format
-  # including the jti, user, and timestamps 
+  # including the jti, user, and timestamps
   def respond_with(resource, _opts = {})
     render json: resource
   end
+
   # upon logout, this is the handler that will handle what to send
   # as response, currently just sets header to 200
   def respond_to_on_destroy
