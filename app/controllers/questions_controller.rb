ca00c2c91cb74b00cbd9648f0a6d2f793b74352c
lee-ma <leema9321@gmail.com>
2019-11-25 21:10:15 -0500

    remove require auth from questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!
   def index
     @questions = Question.all
     render json: @questions
