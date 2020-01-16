91b90b5beedb49e448291394c6d65ff07c530d46
Leon Ouyang <leonouyang417@gmail.com>
2020-01-16 15:34:45 -0500

    Add seed data (#35)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,39 @@
 
 case Rails.env
 when 'development'
-  User.create(email: ENV['SEED_USER_EMAIL'], password: ENV['SEED_USER_PASSWORD'], jti: SecureRandom.uuid)
+  # create seed data
+  User.create!(
+    email: ENV['SEED_USER_EMAIL'],
+    password: ENV['SEED_USER_PASSWORD'],
+    jti: SecureRandom.uuid
+  )
+  flowchart = Flowchart.create!(
+    title: 'Flow Chart!!!!',
+    description: 'this is an awesome flowchart',
+    height: 1
+  )
+  node1 = FlowchartNode.create!(
+    text: 'Node text 1',
+    header: 'Node header 1',
+    button_text: 'Button text 1',
+    next_question: 'Next question 1',
+    is_root: true,
+    flowchart_id: flowchart.id
+  )
+  node2 = FlowchartNode.create!(
+    text: 'Node text 2',
+    header: 'Node header 2',
+    button_text: 'Button text 2',
+    next_question: 'Next question 2',
+    is_root: false,
+    flowchart_id: flowchart.id
+  )
+
+  # set foreign keys
+  flowchart[:root_id] = node1.id
+  node1[:child_id] = node2.id
+
+  # write changes
+  flowchart.save!
+  node1.save!
 end
