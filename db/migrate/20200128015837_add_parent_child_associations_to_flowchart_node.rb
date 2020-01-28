b83ccb30618c2515827c28d5ad1dd304d9c5c3f9
AinleyP <ainleypena@gmail.com>
2020-01-27 21:48:24 -0500

    fixlint

--- db/migrate/20200128015837_add_parent_child_associations_to_flowchart_node.rb
+++ db/migrate/20200128015837_add_parent_child_associations_to_flowchart_node.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddParentChildAssociationsToFlowchartNode < ActiveRecord::Migration[6.0]
   def change
     add_reference :flowchart_nodes, :flowchart_node, foreign_key: true
