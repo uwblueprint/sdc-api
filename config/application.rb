e62971f0c5d9bd696fce03b17d597f7d75bbff55
Peiyao Chen <p72chen@uwaterloo.ca>
2020-03-16 20:14:00 -0400

    remove origins thing

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module SdcApi
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins ENV['CORS_ORIGIN']
         resource(
           '*',
           headers: :any,
