fac7dd07975c266c96e0716147a387cd00bd83a9
LeozMaxwellJilliams <leonouyang417@gmail.com>
2020-04-02 14:47:42 -0400

    change db host to localhost

--- config/initializers/environment.rb
+++ config/initializers/environment.rb
@@ -7,7 +7,7 @@ unless Rails.env.production?
   ENV['DB_PASS_DEV'] = 'sdcdev'
   ENV['DB_USER_DEV'] = 'postgres'
   ENV['DB_NAME_DEV'] = 'sdc'
-  ENV['DB_HOST_DEV'] = 'db'
+  ENV['DB_HOST_DEV'] = 'localhost'
   ENV['DB_PORT_DEV'] = '5432'
   ENV['DB_TIMEOUT_DEV'] = '5000'
   ENV['DB_ADAPTER_DEV'] = 'postgresql'
