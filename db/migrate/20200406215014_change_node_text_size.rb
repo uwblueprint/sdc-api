4f1b7601540bafb437a37fae02c94db04a513480
Leon Ouyang <leonouyang417@gmail.com>
2020-04-06 18:08:00 -0400

    change node text size (#65)
    * change node text size
    
    * remove old tests
    
    * lint

---
+++ db/migrate/20200406215014_change_node_text_size.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ChangeNodeTextSize < ActiveRecord::Migration[6.0]
+  def change
+    change_column :flowchart_nodes, :text, :text
+    change_column_null :flowchart_nodes, :text, true
+  end
+end
